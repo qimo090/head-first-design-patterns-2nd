4bc0c0dceae2f07012d0d35d2dd5881e95e8807e
jiemingpan <grimm9563@gmail.com>
2024-04-06 00:17:03 +0800

    feat(Chapter01): Weapon v1.2
    Character 实例化的时候不设置武器，使用 setWeapon 统一设置 weapon

--- src/pages/Chapter01/Weapon/Character/Character.ts
+++ src/pages/Chapter01/Weapon/Character/Character.ts
@@ -3,10 +3,6 @@ import WeaponBehavior from '@/pages/Chapter01/Weapon/Weapon/WeaponBehavior';
 abstract class Character {
   weapon: WeaponBehavior;
 
-  constructor(w: WeaponBehavior) {
-    this.weapon = w;
-  }
-
   abstract fight(): void;
 
   setWeapon(w: WeaponBehavior) {
--- src/pages/Chapter01/Weapon/index.tsx
+++ src/pages/Chapter01/Weapon/index.tsx
@@ -5,12 +5,14 @@ import BowAndArrow from '@/pages/Chapter01/Weapon/Weapon/BowAndArrow';
 import Knife from '@/pages/Chapter01/Weapon/Weapon/Knife';
 import Sword from '@/pages/Chapter01/Weapon/Weapon/Sword';
 
-const king = new King(new Knife());
+const king = new King();
+king.setWeapon(new Knife());
 king.fight();
 king.setWeapon(new Axe());
 king.fight();
 
-const queen = new Queen(new Sword());
+const queen = new Queen();
+queen.setWeapon(new Sword());
 queen.fight();
 queen.setWeapon(new BowAndArrow());
 queen.fight();
